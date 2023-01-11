e9f6be61585d762acdaab7952a87731802cc895c
chaudhuree <chaudhuree00@gmail.com>
2023-01-12 03:28:02 +0600

    docs: comparison operator and logical operator

--- index.php
+++ index.php
@@ -3,17 +3,63 @@
 ?>
 
 // Single line comment
-        /*
-            This
-            is 
-            a
-            multi
-            line
-            comment
-        */
+/*
+    This
+    is 
+    a
+    multi
+     line
+    comment
+*/
 
 // Variables
 $variable1 = 5;
 $variable2 = 2;
 echo $variable1;
 echo $variable2;
+
+
+// Comparison Operators
+    // echo "<h1> Comparison Operators </h1>";
+    echo "The value of 1==4 is ";
+    echo var_dump(1==4);
+    echo "<br>";
+
+    echo "The value of 1!=4 is ";
+    echo var_dump(1!=4);
+    echo "<br>";
+
+    echo "The value of 1>=4 is ";
+    echo var_dump(1>=4);
+    echo "<br>";
+
+    echo "The value of 1<=4 is ";
+    echo var_dump(1<=4);
+    echo "<br>";
+
+    // Increment/Decrement Operators 
+    // echo $variable1++;
+    // echo $variable1--;
+    // echo ++$variable1;
+    echo --$variable1;
+    echo "<br>";
+    echo $variable1;
+
+    //docs: Logical Operator
+    // and (&&)
+    // or (||)
+    // xor 
+    // !
+
+    // $myVar = (true and true);
+    // $myVar = (false and true);
+    // $myVar = (false and false);
+    // $myVar = (true and false);
+    // $myVar = (true or false);
+
+    // $myVar = (true xor true);
+    // $myVar = (false and true);
+    // $myVar = (false xor false);
+    $myVar = (true and false);
+    echo "<br>";
+    echo var_dump($myVar);
