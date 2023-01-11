ef521d92ada168dc70fb8ea76d0770a5b3d5f8fc
chaudhuree <chaudhuree00@gmail.com>
2023-01-12 04:28:28 +0600

    form handling

---
+++ form handling/form1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>your name is : <?php echo($_POST['name']) ?></p>
+  <p>your email is : <?php echo($_POST['email']) ?></p>
+</body>
+</html>
