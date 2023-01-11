72a2c0e1efb5149a4c4e96689596ac1f4cc782c6
chaudhuree <chaudhuree00@gmail.com>
2023-01-12 03:44:43 +0600

    loops and functions

--- index.php
+++ index.php
@@ -87,3 +87,44 @@ echo $variable2;
     $languages = array("Python", "C++", "php", "NodeJs"); 
     // echo count($languages);   
     // echo $languages[0];   
+
+    //docs: Loops in PHP
+    $a = 0;
+    while ($a <= 10) {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    }
+
+    //docs: Iterating arrays in PHP using while loop
+    $a = 0;
+    while ($a < count($languages)) {
+        echo "<br>The value of language is: ";
+        echo $languages[$a];
+        $a++;
+    }
+
+    //docs: Do while loop
+    $a = 200;
+    do {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    } while ($a < 10);
+
+    //docs: For loop
+    for ($a=60; $a < 10; $a++) { 
+        echo "<br>The value of a from the for loop is: ";
+        echo $a;
+    }
+    //docs: Iterating arrays in PHP using foreach
+    foreach ($languages as $value) {
+        echo "<br>The value from foreach loop is ";
+        echo $value;
+    }
+
+    //docs: Functions in PHP
+    function print_number($number){
+        echo "<br>Your number is ";
+        echo $number;
+    }
