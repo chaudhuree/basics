5b50a56ebf2b2b70fb49999b9796f0d290e2c517
chaudhuree <chaudhuree00@gmail.com>
2023-01-11 23:04:29 +0600

    coment

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
         echo "Hello world and this is printed using PHP";
 ?>
+
+// Single line comment
+        /*
+            This
+            is 
+            a
+            multi
+            line
+            comment
+        */
