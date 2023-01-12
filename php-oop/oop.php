345a61066195b08565b388ad422ee0cc0650c5b9
chaudhuree <chaudhuree00@gmail.com>
2023-01-12 21:56:41 +0600

    another way to instantiate a new person object

--- php-oop/oop.php
+++ php-oop/oop.php
@@ -25,10 +25,34 @@ class Person
 // instantiate a new person object
 $person1 = new Person('John Doe', '
 ', 35);
-
 // echo $person1->name;
 // echo $person1->email;
 // echo $person1->age;
 
 echo $person1->greet();
+
+// another way to instantiate a new person object
+class Person2
+{
+  // properties
+  public $name;
+  public $email;
+  public $age;
+
+  // constructor
+  public function __construct()
+  {
+    echo 'Constructor Called';
+  }
+
+  // methods
+  public function greet()
+  {
+    return "Hello, my name is $this->name and I am $this->age";
+  }
+}
+$person2 = new Person2();
+$person2->name = "sohan";
+$person2->email = 'ch@gmail.com';
+$person2->age = 40;
 ?>
