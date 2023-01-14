7c4a162871b4075d8d0da4bdb928a1173a49b1bb
chaudhuree <chaudhuree00@gmail.com>
2023-01-15 04:11:55 +0600

    spl_autoload_register

--- php-oop/magicmethods/autoload/autoload-method.php
+++ php-oop/magicmethods/autoload/autoload-method.php
@@ -1,3 +1,4 @@
+<!-- the spl_autoload_register() function is used to register a function that is automatically called whenever a new class is used in the code but has not yet been defined -->
 <?php
 
 spl_autoload_register(function ($class) {
