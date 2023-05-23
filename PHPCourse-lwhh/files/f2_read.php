ace2933f86ba4d9602ba59307128fe64f46471cb
chaudhuree <chaudhuree00@gmail.com>
2023-05-23 22:11:30 +0600

    windows file read path setting tips

--- PHPCourse-lwhh/files/f2_read.php
+++ PHPCourse-lwhh/files/f2_read.php
@@ -10,4 +10,7 @@ print_r($fp) . PHP_EOL;
 echo $filecontent . PHP_EOL;
 
 
-echo is_readable($fileData) ? 'Yes,this file is redable' : 'Not redable' . PHP_EOL;
+echo is_readable($fileData) ? "Yes,this file is redable\n" : 'Not redable' . PHP_EOL;
+
+$anotherpath="d:\\codes\\php\\basics\\PHPCourse-lwhh\\files\\data\\data.txt";
+echo is_readable($anotherpath) ? "Yes,this file is redable\n" : 'Not redable' . PHP_EOL;
