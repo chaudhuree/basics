2442fe016057c3ae9e47fe486d094705fb099061
chaudhuree <chaudhuree00@gmail.com>
2023-06-22 21:28:50 +0600

    pdo connection setup

---
+++ pdo/connect.php
@@ -0,0 +1,57 @@
+<?php
+// for connection
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+// for error handling
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// for query and fetching data
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC); //it will fetch all data as associative array
+
+// echo '<pre>';
+// var_dump($products);
+// echo '</pre>';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php pdo connection and data show</title>
+</head>
+
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Create Date</th>
+
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($products as $i => $product) { ?>
+        <tr>
+          <th scope="row"><?php echo $i + 1 ?></th>
+          <td>
+            <?php if ($product['image']) : ?>
+              <img src="<?php echo $product['image'] ?>" alt="<?php echo $product['title'] ?>" class="product-img">
+            <?php endif; ?>
+          </td>
+          <td><?php echo $product['title'] ?></td>
+          <td><?php echo $product['price'] ?></td>
+          <td><?php echo $product['create_date'] ?></td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+</body>
+
+</html>
